1926d31cd7e1733e8c85e17b2726f115144ed3f6
Victor Navascues <navascues.victor@gmail.com>
2022-11-18 08:11:41 +0100

    feat(drcoordintor): updated CryptoCompare contract

--- contracts/drcoordinator/v0.8/dev/DRCConsumerCryptoCompare.sol
+++ contracts/drcoordinator/v0.8/dev/DRCConsumerCryptoCompare.sol
@@ -6,12 +6,24 @@ import { Chainlink, DRCoordinatorClient, IDRCoordinator } from "../DRCoordinator
 contract DRCConsumerCryptoCompare is DRCoordinatorClient {
     using Chainlink for Chainlink.Request;
 
-    struct PriceData {
+    struct PriceData3 {
         uint256 btc;
         uint256 eth;
         uint256 link;
     }
-    mapping(bytes32 => PriceData) public requestIdToPriceData;
+
+    struct PriceData6 {
+        uint256 btc;
+        uint256 eth;
+        uint256 link;
+        uint256 matic;
+        uint256 aave;
+        uint256 snx;
+    }
+
+    mapping(bytes32 => uint256) public requestIdToPriceData1;
+    mapping(bytes32 => PriceData3) public requestIdToPriceData3;
+    mapping(bytes32 => PriceData6) public requestIdToPriceData6;
 
     error LinkTransferFailed(address to, uint256 amount);
 
@@ -28,26 +40,44 @@ contract DRCConsumerCryptoCompare is DRCoordinatorClient {
         s_drCoordinator.cancelRequest(_requestId);
     }
 
-    function fulfillPrices(
+    // Function signature: d276286e
+    function fulfillPriceData1(bytes32 _requestId, uint256 _link) external recordFulfillment(_requestId) {
+        requestIdToPriceData1[_requestId] = _link;
+    }
+
+    // Function signature: 3551fb7a
+    function fulfillPriceData3(
         bytes32 _requestId,
         uint256 _btc,
         uint256 _eth,
         uint256 _link
     ) external recordFulfillment(_requestId) {
-        PriceData memory priceData;
-        priceData.btc = _btc;
-        priceData.eth = _eth;
-        priceData.link = _link;
-        requestIdToPriceData[_requestId] = priceData;
+        PriceData3 memory priceData3 = PriceData3(_btc, _eth, _link);
+        requestIdToPriceData3[_requestId] = priceData3;
+    }
+
+    // Function signature: ea4ed058
+    function fulfillPriceData6(
+        bytes32 _requestId,
+        uint256 _btc,
+        uint256 _eth,
+        uint256 _link,
+        uint256 _matic,
+        uint256 _aave,
+        uint256 _snx
+    ) external recordFulfillment(_requestId) {
+        PriceData6 memory priceData6 = PriceData6(_btc, _eth, _link, _matic, _aave, _snx);
+        requestIdToPriceData6[_requestId] = priceData6;
     }
 
     function requestPrices(
         address _operatorAddr,
         bytes32 _specId,
         uint32 _callbackGasLimit,
-        uint96 _consumerMaxPayment
+        uint96 _consumerMaxPayment,
+        bytes4 _callbackFunctionId
     ) external {
-        Chainlink.Request memory req = _buildRequest(_specId, address(this), this.fulfillPrices.selector);
+        Chainlink.Request memory req = _buildRequest(_specId, address(this), _callbackFunctionId);
         _sendRequest(_operatorAddr, _callbackGasLimit, _consumerMaxPayment, req);
     }
 
@@ -66,8 +96,16 @@ contract DRCConsumerCryptoCompare is DRCoordinatorClient {
 
     /* ========== EXTERNAL VIEW FUNCTIONS ========== */
 
-    function getPriceData(bytes32 _requestId) external view returns (PriceData memory) {
-        return requestIdToPriceData[_requestId];
+    function getPriceData1(bytes32 _requestId) external view returns (uint256) {
+        return requestIdToPriceData1[_requestId];
+    }
+
+    function getPriceData3(bytes32 _requestId) external view returns (PriceData3 memory) {
+        return requestIdToPriceData3[_requestId];
+    }
+
+    function getPriceData6(bytes32 _requestId) external view returns (PriceData6 memory) {
+        return requestIdToPriceData6[_requestId];
     }
 
     /* ========== PRIVATE PURE FUNCTIONS ========== */
